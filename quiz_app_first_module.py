496efbb04726671420afbdd894dad7fd431a8f42
himanshu <kashking474@gmail.com>
2022-08-17 21:02:03 +0530

    some-updates

---
+++ quiz_app_first_module.py
@@ -0,0 +1,31 @@
+'''
+Created on 17-Aug-2022
+
+@author: HP
+''' 
+
+
+"""list_QA =[
+                ("When was the first known use of the word 'quiz'","1781"),
+                ("which python library is used for arrays","Numpy")
+    ]"""
+
+dict_QA = {
+        "When was the first known use of the word 'quiz'":["1781","1780","1880","1881"],
+        "which python library is used for arrays":["numpy","pandas","scikit","matplotlib"],
+        "What's the name of Python's sorting algorithm": ["Timsort", "Quicksort", "Merge sort", "Bubble sort"]
+    }
+
+for ques,choices in dict_QA.items():
+    correct_ans = choices[0]
+    sorted_alternative = sorted(choices)
+    for label,choice in enumerate(sorted_alternative):
+        print(f" {label}) {choice}")
+    
+    
+    user_label= int(input(f"{ques}?"))
+    user_ans = sorted_alternative[user_label]
+    if user_ans == correct_ans:
+        print("Correct,nicely done")
+    else:
+        print(f"The correct answer is: {correct_ans!r},not {user_ans!r}")
